aee65525294e90a6214ac828cbdb696de8317097
Ben-G <Benjamin.Encz@gmail.com>
2015-08-27 10:43:27 -0700

    [POST Trip] Add first test

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
-
+mongo = PyMongo(app)
 api = Api(app)
 
 trips = {}
 
 class Trip(Resource):
     def get(self, trip_id):
-        return {trips[trip_id]}
+        return trips[trip_id]
 
     def post(self, trip_id):
-        trips[trip_id] = request.form['data']
-        return {trip_id: trips[trip_id]}
+      trips[trip_id] = request.json
+      
+      return request.json
 
 api.add_resource(Trip, '/trip/<string:trip_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(debug=True)
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import os
 import server
 import unittest
 import tempfile
+import json
 
 class FlaskrTestCase(unittest.TestCase):
 
@@ -13,13 +14,21 @@ class FlaskrTestCase(unittest.TestCase):
       print("teardown")
 
     def test_posting_trip(self):
-      response = self.app.post('/trip/1', data=dict(
-        name="Stuttgart Roadtrip"
-      ))
+      response = self.app.post('/trip/1', 
+        data=json.dumps(dict(
+          name="Stuttgart Roadtrip"
+        )), 
+        content_type = 'application/json')
+
+      assert 'Stuttgart Roadtrip' in response.data.decode()
+
+    def test_getting_trip(self):
+      response = self.app.get('/trip/1')
 
       print(response.data.decode())
 
       assert 'Stuttgart Roadtrip' in response.data.decode()
+
 
 if __name__ == '__main__':
     unittest.main()
