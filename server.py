62c431c6a12fada69afe29c13d1fb08e5929d783
Ben-G <Benjamin.Encz@gmail.com>
2015-09-24 10:24:04 +0200

    [Style] Apply PEP8 Autoformat

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-import json
 import bcrypt
-from flask import Flask, request, make_response, Response, jsonify
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 from utils.mongo_json_encoder import JSONEncoder
@@ -13,92 +12,97 @@ app.db = mongo.develop_database
 app.bcrypt_rounds = 12
 api = Api(app)
 
+
 def check_auth(username, password):
     user_collection = app.db.users
     user = user_collection.find_one({'username': username})
 
     if user is None:
-      return False
-    else:
-      # check if the hash we generate based on auth matches stored hash
-      encodedPassword = password.encode('utf-8')
-      if bcrypt.hashpw(encodedPassword, user['password']) == user['password']:
-        return True
-      else: 
         return False
+    else:
+        # check if the hash we generate based on auth matches stored hash
+        encodedPassword = password.encode('utf-8')
+        if bcrypt.hashpw(encodedPassword, user['password']) == user['password']:
+            return True
+        else:
+            return False
+
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
-          message = {'error': 'Basic Auth Required.'}
-          resp = jsonify(message)
-          resp.status_code = 401
-          return resp
+            message = {'error': 'Basic Auth Required.'}
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
 
         return f(*args, **kwargs)
     return decorated
 
+
 class User(Resource):
 
     def post(self):
-      if (request.json['username'] is None or request.json['password'] is None):
-        message = {'error':  'Request requires username and password'}
-        resp = jsonify(message)
-        resp.status_code = 400
-        return resp
+        if (request.json['username'] is None or request.json['password'] is None):
+            message = {'error':  'Request requires username and password'}
+            resp = jsonify(message)
+            resp.status_code = 400
+            return resp
 
-      user_collection = app.db.users
-      user = user_collection.find_one({'username': request.json['username']})
+        user_collection = app.db.users
+        user = user_collection.find_one({'username': request.json['username']})
 
-      if user is not None:
-        message = {'error': 'Username already in use'}
-        resp = jsonify(message)
-        resp.status_code = 400
-        return resp
-      else:
-        encodedPassword = request.json['password'].encode('utf-8')
-        hashed = bcrypt.hashpw(encodedPassword, bcrypt.gensalt(app.bcrypt_rounds))
-        request.json['password'] = hashed
-        user_collection.insert_one(request.json)
+        if user is not None:
+            message = {'error': 'Username already in use'}
+            resp = jsonify(message)
+            resp.status_code = 400
+            return resp
+        else:
+            encodedPassword = request.json['password'].encode('utf-8')
+            hashed = bcrypt.hashpw(
+                encodedPassword, bcrypt.gensalt(app.bcrypt_rounds))
+            request.json['password'] = hashed
+            user_collection.insert_one(request.json)
 
     @requires_auth
     def get(self):
-      # if request handler is implemented, auth was successful
-      response = jsonify(data=[])
-      response.status_code = 200
-      return response
+        # if request handler is implemented, auth was successful
+        response = jsonify(data=[])
+        response.status_code = 200
+        return response
 
 
 class Trip(Resource):
 
     @requires_auth
-    def get(self, trip_id = None):
-      if trip_id is None:
-        trip_collection = app.db.trips
-        trips = list(trip_collection.find({'user': request.authorization.username}))
-        return trips
-      else:
-        trip_collection = app.db.trips
-        trip = trip_collection.find_one(
-          {'_id': ObjectId(trip_id), 
-          'user': request.authorization.username}
-        )
-        return trip
+    def get(self, trip_id=None):
+        if trip_id is None:
+            trip_collection = app.db.trips
+            trips = list(trip_collection.find(
+                {'user': request.authorization.username}))
+            return trips
+        else:
+            trip_collection = app.db.trips
+            trip = trip_collection.find_one(
+                {'_id': ObjectId(trip_id),
+                 'user': request.authorization.username}
+            )
+            return trip
 
     @requires_auth
     def post(self):
-      new_trip = request.json
-      new_trip['user'] = request.authorization.username
-      trip_collection = app.db.trips
-      result = trip_collection.insert_one(request.json)
+        new_trip = request.json
+        new_trip['user'] = request.authorization.username
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(request.json)
 
-      trip = trip_collection.find_one(result.inserted_id)
+        trip = trip_collection.find_one(result.inserted_id)
 
-      return trip
+        return trip
 
-api.add_resource(Trip, '/trip/','/trip/<string:trip_id>')
+api.add_resource(Trip, '/trip/', '/trip/<string:trip_id>')
 api.add_resource(User, '/user/')
 
 
@@ -110,6 +114,7 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
+    # Turn this on in debug mode to get detailled information about request
+    # related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
--- tests.py
+++ tests.py
@@ -1,190 +1,191 @@
-import os
 import server
 import unittest
-import tempfile
 import json
 import base64
 from pymongo import MongoClient
 
 db = None
 
+
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-      self.app = server.app.test_client()
-      # Run app in testing mode to retrieve exceptions and stack traces
-      server.app.config['TESTING'] = True
+        self.app = server.app.test_client()
+        # Run app in testing mode to retrieve exceptions and stack traces
+        server.app.config['TESTING'] = True
 
-      # Inject test database into application
-      mongo = MongoClient('localhost', 27017)
-      global db
-      db = mongo.test_database
-      server.app.db = db
+        # Inject test database into application
+        mongo = MongoClient('localhost', 27017)
+        global db
+        db = mongo.test_database
+        server.app.db = db
 
-      # Reduce encryption workloads for tests
-      server.app.bcrypt_rounds = 4
+        # Reduce encryption workloads for tests
+        server.app.bcrypt_rounds = 4
 
-      # Drop collection (significantly faster than dropping entire db)
-      db.drop_collection('trips')
+        # Drop collection (significantly faster than dropping entire db)
+        db.drop_collection('trips')
 
-      self.app.post('/user/', 
-        data = json.dumps(dict(
-          username = "user",
-          password = "password"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/user/',
+                      data=json.dumps(dict(
+                          username="user",
+                          password="password"
+                      )),
+                      content_type='application/json')
 
     @classmethod
     def tearDownClass(cls):
-      db.drop_collection('users')
+        db.drop_collection('users')
 
     # User tests
 
     def test_signup_with_username_and_password(self):
-      response = self.app.post('/user/', 
-        data = json.dumps(dict(
-          username = "benjamin",
-          password = "test"
-        )), 
-        content_type = 'application/json')
+        response = self.app.post('/user/',
+                                 data=json.dumps(dict(
+                                     username="benjamin",
+                                     password="test"
+                                 )),
+                                 content_type='application/json')
 
-      responseJSON = json.loads(response.data.decode())
+        json.loads(response.data.decode())
 
-      self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_verifying_credentials(self):
-      self.app.post('/user/', 
-        data = json.dumps(dict(
-          username = "benjamin",
-          password = "test"
-        )), 
-        content_type = 'application/json')   
+        self.app.post('/user/',
+                      data=json.dumps(dict(
+                          username="benjamin",
+                          password="test"
+                      )),
+                      content_type='application/json')
 
-      response = self.app.get('/user/', 
-        headers=self.default_auth_header()
-        )
+        response = self.app.get('/user/',
+                                headers=self.default_auth_header()
+                                )
 
-      self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_incorrect_credentials(self):
-      response = self.app.get('/user/', 
-        headers=self.generate_auth_header('wrongusername', 'andpassword')
-        )      
+        response = self.app.get('/user/',
+                                headers=self.generate_auth_header(
+                                    'wrongusername', 'andpassword')
+                                )
 
-      self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 401)
 
     # Trip tests
 
     def test_posting_trip(self):
-      response = self.app.post('/trip/', 
-        headers=self.default_auth_header(),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
-      
-      responseJSON = json.loads(response.data.decode())
+        response = self.app.post('/trip/',
+                                 headers=self.default_auth_header(),
+                                 data=json.dumps(dict(
+                                     name="Stuttgart Roadtrip"
+                                 )),
+                                 content_type='application/json')
 
-      self.assertEqual(response.status_code, 200)
-      assert 'application/json' in response.content_type
-      assert 'Stuttgart Roadtrip' in responseJSON["name"]
-      assert 'user' in responseJSON["user"]
+        responseJSON = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 200)
+        assert 'application/json' in response.content_type
+        assert 'Stuttgart Roadtrip' in responseJSON["name"]
+        assert 'user' in responseJSON["user"]
 
     def test_unauthorized_request(self):
-      response = self.app.post('/trip/', 
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
-      self.assertEqual(response.status_code, 401)
+        response = self.app.post('/trip/',
+                                 data=json.dumps(dict(
+                                     name="Stuttgart Roadtrip"
+                                 )),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 401)
 
     def test_getting_trip(self):
-      response = self.app.post('/trip/', 
-        headers=self.default_auth_header(),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
+        response = self.app.post('/trip/',
+                                 headers=self.default_auth_header(),
+                                 data=json.dumps(dict(
+                                     name="Stuttgart Roadtrip"
+                                 )),
+                                 content_type='application/json')
 
-      postResponseJSON = json.loads(response.data.decode())
-      postedObjectID = postResponseJSON["_id"]
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
 
-      response = self.app.get('/trip/'+postedObjectID,
-          headers=self.default_auth_header()
-        )
-      responseJSON = json.loads(response.data.decode())
+        response = self.app.get('/trip/' + postedObjectID,
+                                headers=self.default_auth_header()
+                                )
+        responseJSON = json.loads(response.data.decode())
 
-      self.assertEqual(response.status_code, 200)
-      assert 'Stuttgart Roadtrip' in responseJSON["name"]
+        self.assertEqual(response.status_code, 200)
+        assert 'Stuttgart Roadtrip' in responseJSON["name"]
 
     def test_getting_all_trips(self):
-      self.app.post('/trip/', 
-        headers=self.default_auth_header(),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/trip/',
+                      headers=self.default_auth_header(),
+                      data=json.dumps(dict(
+                          name="Stuttgart Roadtrip"
+                      )),
+                      content_type='application/json')
 
-      self.app.post('/trip/', 
-        headers=self.default_auth_header(),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/trip/',
+                      headers=self.default_auth_header(),
+                      data=json.dumps(dict(
+                          name="Stuttgart Roadtrip"
+                      )),
+                      content_type='application/json')
 
-      response = self.app.get('/trip/',
-          headers=self.default_auth_header()
-        )
-      responseJSON = json.loads(response.data.decode())
+        response = self.app.get('/trip/',
+                                headers=self.default_auth_header()
+                                )
+        responseJSON = json.loads(response.data.decode())
 
-      self.assertEqual(response.status_code, 200)
-      self.assertEqual(len(responseJSON), 2)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(responseJSON), 2)
 
-    
     def test_getting_all_trips_is_user_specific(self):
-      self.app.post('/trip/', 
-        headers=self.default_auth_header(),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/trip/',
+                      headers=self.default_auth_header(),
+                      data=json.dumps(dict(
+                          name="Stuttgart Roadtrip"
+                      )),
+                      content_type='application/json')
 
-      self.app.post('/user/', 
-        data = json.dumps(dict(
-          username = "user2",
-          password = "test"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/user/',
+                      data=json.dumps(dict(
+                          username="user2",
+                          password="test"
+                      )),
+                      content_type='application/json')
 
-      self.app.post('/trip/', 
-        headers=self.generate_auth_header('user2', 'test'),
-        data=json.dumps(dict(
-          name="Stuttgart Roadtrip"
-        )), 
-        content_type = 'application/json')
+        self.app.post('/trip/',
+                      headers=self.generate_auth_header('user2', 'test'),
+                      data=json.dumps(dict(
+                          name="Stuttgart Roadtrip"
+                      )),
+                      content_type='application/json')
 
-      responseUser1 = self.app.get('/trip/',
-          headers=self.default_auth_header()
-        )
-      responseUser1JSON = json.loads(responseUser1.data.decode())
+        responseUser1 = self.app.get('/trip/',
+                                     headers=self.default_auth_header()
+                                     )
+        responseUser1JSON = json.loads(responseUser1.data.decode())
 
-      responseUser2 = self.app.get('/trip/',
-          headers=self.generate_auth_header('user2', 'test'),
-        )
-      responseUser2JSON = json.loads(responseUser2.data.decode())
+        responseUser2 = self.app.get('/trip/',
+                                     headers=self.generate_auth_header(
+                                         'user2', 'test'),
+                                     )
+        responseUser2JSON = json.loads(responseUser2.data.decode())
 
-      self.assertEqual(responseUser1.status_code, 200)
-      self.assertEqual(len(responseUser1JSON), 1)
+        self.assertEqual(responseUser1.status_code, 200)
+        self.assertEqual(len(responseUser1JSON), 1)
 
-      self.assertEqual(responseUser2.status_code, 200)
-      self.assertEqual(len(responseUser2JSON), 1)
+        self.assertEqual(responseUser2.status_code, 200)
+        self.assertEqual(len(responseUser2JSON), 1)
 
     def default_auth_header(self):
-      return self.generate_auth_header("user", "password")
+        return self.generate_auth_header("user", "password")
 
     def generate_auth_header(self, username, password):
-      authString = username + ":" + password
-      return {'Authorization': 'Basic ' + base64.b64encode(authString.encode('utf-8')).decode('utf-8') }
+        authString = username + ":" + password
+        return {'Authorization': 'Basic ' +
+                base64.b64encode(authString.encode('utf-8')).decode('utf-8')}
 
 if __name__ == '__main__':
     unittest.main()
